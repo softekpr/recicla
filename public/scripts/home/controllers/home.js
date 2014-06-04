adfab416bfca8fcfcffe2271df061771e3c90cc5
Luis Catoni <psychoff@gmail.com>
2014-06-04 11:50:09 -0400

    custom icons + layout stuff

--- public/scripts/home/controllers/home.js
+++ public/scripts/home/controllers/home.js
@@ -2,8 +2,19 @@
     'use strict';
 
     angular.module('recicla')
-        .controller('HomeController', function($scope) {
+        .controller('HomeController', function($scope, $rootScope, $window) {
             $scope.message = 'Hello Dude!';
+
+            var resizeElements = function () {
+                var mapHeight = angular.element($window).height() - angular.element('#navigation').height();
+                angular.element('#map .angular-google-map-container').height(mapHeight);
+                angular.element('#desktop-menu').height(angular.element($window).height());
+            };
+
+            $scope.$on('$viewContentLoaded', function () {
+                angular.element($window).bind('resize', resizeElements);
+                resizeElements();
+            });
         })
         .config(function($routeProvider) {
             $routeProvider.when('/', {
