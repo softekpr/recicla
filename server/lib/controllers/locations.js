0df123ce9257b13fdef5c94c22121e2e4d725c13
JOSE HERNANDEZLUCENA <jhernandez@softekpr.com>
2014-06-04 11:09:29 -0400

    Fix var test error

--- server/lib/controllers/locations.js
+++ server/lib/controllers/locations.js
@@ -11,7 +11,7 @@
     LocationController.prototype.getLocations = function (req, res) {
         Material.find().where('name').in(req.body.materials)
             .exec(function (err, result) {
-                var test = new User({coordinates: req.body.userLocation, materials: result});
+                //var test = new User({coordinates: req.body.userLocation, materials: result});
 
                 new User({coordinates: req.body.userLocation, materials: result})
                     .save(function (err) {
