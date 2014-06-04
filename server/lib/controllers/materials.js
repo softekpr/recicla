8ed1a2c9ba8e765361d590c22c136ebb3614fbaf
JOSE HERNANDEZLUCENA <jhernandez@softekpr.com>
2014-06-04 10:43:41 -0400

    -a

---
+++ server/lib/controllers/materials.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    var Material = require('../models/material.js');
+
+    var MaterialController = function(){};
+
+    MaterialController.prototype.getMaterials = function(req, res) {
+        Material.find().exec(function(err, materials) {
+            if(err) {
+                res.send(500, err);
+            } else {
+                res.json(materials);
+            }
+        });
+    };
+
+    module.exports = new MaterialController();
+})();
