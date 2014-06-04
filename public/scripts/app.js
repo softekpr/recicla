1f17c496d2609b9da0ccda5df087ffe6af82061f
Luis Catoni <psychoff@gmail.com>
2014-06-04 09:45:17 -0400

    app modules

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('recicla', ['ngRoute', 'ngResource', 'ui.bootstrap'])
+    angular.module('recicla', ['ngRoute', 'ngResource', 'ngSanitize', 'ngAnimate', 'ui.bootstrap', 'mgcrea.ngStrap.modal', 'mgcrea.ngStrap.aside', 'mgcrea.ngStrap.button', 'geolocation', 'google-maps'])
         .config(function($routeProvider, $locationProvider) {
             $locationProvider.html5Mode(true);
 
